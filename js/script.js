6b10871e29edd1e1e20a288f0ed1a241876472ed
Michał Szymański <voiceofthunder0108@gmail.com>
2023-03-07 00:56:00 +0100

    Add class active to link after click

--- js/script.js
+++ js/script.js
@@ -5,19 +5,22 @@
   });
 */
 const titleClickHandler = function(event){
+    const clickedElement = this;
     console.log('Link was clicked!');
-    console.log(event);
   
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
+
+    /* [DONE] add class 'active' to the clicked link */
+
+console.log('clickedElement:', clickedElement);
+clickedElement.classList.add('active');
   
-    /* add class 'active' to the clicked link */
-  
-    /* remove class 'active' from all articles */
+    /* [DONE] remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.posts article.active');
 
 for(let activeArticle of activeArticles){
