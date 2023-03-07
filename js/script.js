75e44651d02660b96333a233c7a168945b327189
Michał Szymański <voiceofthunder0108@gmail.com>
2023-03-07 13:16:33 +0100

    Fix code formatting

--- js/script.js
+++ js/script.js
@@ -10,32 +10,31 @@ const titleClickHandler = function(event){
     console.log('Link was clicked!');
   
     /* [DONE] remove class 'active' from all article links  */
-const activeLinks = document.querySelectorAll('.titles a.active');
+    const activeLinks = document.querySelectorAll('.titles a.active');
 
-for(let activeLink of activeLinks){
-  activeLink.classList.remove('active');
-}
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
 
     /* [DONE] add class 'active' to the clicked link */
-
-console.log('clickedElement:', clickedElement);
-clickedElement.classList.add('active');
-  
+    console.log('clickedElement:', clickedElement);
+    clickedElement.classList.add('active');
+    
     /* [DONE] remove class 'active' from all articles */
-const activeArticles = document.querySelectorAll('.posts article.active');
+    const activeArticles = document.querySelectorAll('.posts article.active');
 
-for(let activeArticle of activeArticles){
-    activeArticle.classList.remove('active');
-}
-  
+    for(let activeArticle of activeArticles){
+        activeArticle.classList.remove('active');
+    }
+    
     /* [DONE] get 'href' attribute from the clicked link */
-const articleSelector = clickedElement.getAttribute('href');
-  
+    const articleSelector = clickedElement.getAttribute('href');
+    
     /* [DONE] find the correct article using the selector (value of 'href' attribute) */
-const targetArticle = document.querySelector(articleSelector);
-  
+    const targetArticle = document.querySelector(articleSelector);
+    
     /* [DONE] add class 'active' to the correct article */
-targetArticle.classList.add('active');
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
